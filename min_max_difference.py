ef3726c9ef5d196ac6a3020dc94c8262773a8dab
Greg Spokas <Gregnspokas@gmail.com>
2019-10-29 19:16:20 -0400

    First live coding

---
+++ min_max_difference.py
@@ -0,0 +1,14 @@
+import math
+
+the_array = [15,22,84,14,88,23]
+
+def min():
+    return min(the_array)
+
+
+def max():
+    return max(the_array)
+
+def difference():
+
+     return max(the_array) - min(the_array)
